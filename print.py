27fdf0b2ab896e55cbc5f48c549bd57828895656
Nikhil Kumar <91002892+yadavnikhilrao@users.noreply.github.com>
2022-10-15 13:38:59 -0600

    Add files via upload

---
+++ print.py
@@ -0,0 +1,8 @@
+'''The included code stub will read an integer, n, from STDIN.
+Without using any string methods, try to print the following: 123...n 
+Note that "..." represents the consecutive values in between.'''
+
+
+n = int(input())
+for i in range(1,n+1):
+    print(i, end= "")
